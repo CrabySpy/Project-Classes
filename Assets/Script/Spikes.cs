88716def4760d973a5550268754e51262ca7fdb1
steven <steven.wong26@ycdsbk12.ca>
2025-03-28 22:00:08 -0400

    Knockback (Death animation)

--- Assets/Script/Spikes.cs
+++ Assets/Script/Spikes.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 public class Spike : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private float knockbackForce = 5f;
+    [SerializeField] private float respawnDelay = 0.5f; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = respawnPoint.position;
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 knockbackDirection = (other.transform.position - transform.position).normalized;
+                rb.linearVelocity = knockbackDirection * knockbackForce;
+            }
+
+            Invoke(nameof(RespawnPlayer), respawnDelay);
+        }
+    }
+
+    private void RespawnPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = respawnPoint.position;
         }
     }
 }
